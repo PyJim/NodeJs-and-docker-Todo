4c037f083da4e860133120ca4326ebf993133be0
Jimmy Essel <jimmykodwoessel@gmail.com>
2025-02-05 11:11:34 +0000

    chore: added tests

---
+++ todo-app/tests/test.js
@@ -0,0 +1,24 @@
+describe('Dummy Tests', () => {
+
+    test('should always return true - Test 1', () => {
+      expect(true).toBe(true);
+    });
+  
+    test('should always return true - Test 2', () => {
+      expect(true).toBe(true);
+    });
+  
+    test('should always return true - Test 3', () => {
+      expect(true).toBe(true);
+    });
+  
+    test('should always return true - Test 4', () => {
+      expect(true).toBe(true);
+    });
+  
+    test('should always return true - Test 5', () => {
+      expect(true).toBe(true);
+    });
+  
+  });
+  
